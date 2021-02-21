0037b89b4d798a9aab482a021c480e6b7cf3be31
zephyrus21 <pandey.piyus@gmail.com>
2021-02-22 01:21:51 +0530

    adding report logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
 import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf8',
+  })
+  .split('\n')
+  .map((line: string): string[] => line.split(','));
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let MUW = 0;
+
+for (const match of matches) {
+  if (
+    (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
+    (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
+  )
+    MUW++;
+}
+
+console.log(MUW);
