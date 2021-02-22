6cb029bbfc14b82be1a8de6f6988866840bbdc9e
zephyrus21 <pandey.piyus@gmail.com>
2021-02-22 11:59:29 +0530

    adding generics way

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,3 +1,5 @@
+/*
+? Original
 import fs from 'fs';
 import { dateStringToDate } from './Utils';
 import { MatchResult } from './MatchResult';
@@ -31,4 +33,28 @@ export class CsvFileReader {
         ]
       );
   }
+}
+*/
+
+import fs from 'fs';
+
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
+
+  constructor(public fileName: string) {}
+
+  abstract mapRow(row: string[]): T;
+
+  read = (): void => {
+    this.data = fs
+      .readFileSync(this.fileName, {
+        encoding: 'utf8',
+      })
+      //! split the file into lines
+      .split('\n')
+      //! map all the values separated by ,
+      .map((line: string): string[] => line.split(','))
+      //! to get all the value and convert them to suitable type
+      .map(this.mapRow);
+  };
 }
