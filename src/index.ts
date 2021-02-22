11c4a0b40395bea752521a6300eb637daefa4d90
zephyrus21 <pandey.piyus@gmail.com>
2021-02-22 19:13:30 +0530

    adding html report

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { WinAnalysis } from './analyzers/WinAnalysis';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { ConsoleReport } from './report/consoleReport';
+import { ConsoleReport } from './report/ConsoleReport';
+import { HtmlReport } from './report/HtmlReport';
 import { Summary } from './Summary';
 
 const reader = new CsvFileReader('football.csv');
@@ -10,5 +11,10 @@ const matchReader = new MatchReader(reader);
 matchReader.load();
 
 const summary = new Summary(new WinAnalysis('Man United'), new ConsoleReport());
+const summaryHtml = new Summary(
+  new WinAnalysis('Man United'),
+  new HtmlReport()
+);
 
 summary.buildAndReport(matchReader.matches);
+summaryHtml.buildAndReport(matchReader.matches);
